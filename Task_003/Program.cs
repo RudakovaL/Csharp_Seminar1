53be4d32d8a41876c7925bb05a53615c78cffebe
liubov <Madamlub98@yandex.ru>
2024-08-24 00:06:51 +0300

    Добавили решение задачи 3

---
+++ Task_003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите метод, который на вход принимает число и выводит, является ли
+// оно положительным (больше нуля), отрицательным (меньше нуля) или нулём.
+
+using System;
+class Answer {
+static void CheckNumber(int number)
+{
+if (number > 0)
+{
+Console.WriteLine("положительное");
+}
+else if (number < 0)
+{
+Console.WriteLine("отрицательное");
+}
+else
+{
+Console.WriteLine("ноль");
+}
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 7;
+}
+CheckNumber(number);
+}
+}
