cc3ee329e19c988443e9eb30cfd80b21ad40fe2f
liubov <Madamlub98@yandex.ru>
2024-08-24 00:09:40 +0300

    Добавили решение задачи 4

---
+++ Task_004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите метод, который на вход принимает число (number), а на выходе
+// выводит все нечетные числа от 1 до number (включительно), после каждого числа
+// должен быть знак пробела.
+
+using System;
+public class Answer {
+static void PrintOddNumbers(int number)
+{
+for (int i = 1; i <= number; i += 2)
+{
+Console.Write(i + " ");
+}
+Console.WriteLine();
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 5;
+}
+PrintOddNumbers(number);
+}
+}
